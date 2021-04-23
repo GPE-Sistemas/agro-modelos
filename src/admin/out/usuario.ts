7db6819a0c46a5472f6757c202773298848e21a2
Fernando Cortese <cortesefernando@gmail.com>
2021-04-23 19:49:27 -0300

    quitada clave de usuario dto

--- src/admin/out/usuario.ts
+++ src/admin/out/usuario.ts
@@ -4,7 +4,6 @@ import j2s from 'joi-to-swagger';
 export const IUsuarioDTOValidation = joi.object<IUsuarioDTO>({
     _id: joi.string(),
     usuario: joi.string().required(),
-    clave: joi.string().required(),
     idCliente: joi.string().required(),
 });
 
@@ -13,6 +12,5 @@ export const IUsuarioDTOSwagger = j2s(IUsuarioDTOValidation).swagger;
 export interface IUsuarioDTO {
     _id: string;
     usuario: string;
-    clave: string;
     idCliente: string;
 }
