0278e68d50a5f38fcf71984c17bfc50084dfd037
Fernando Cortese <cortesefernando@gmail.com>
2021-04-24 12:54:20 -0300

    modelos chirpstack

---
+++ src/modelos/agro/index.ts
@@ -0,0 +1,84 @@
+export * from './in/animal';
+export * from './mongo/animal';
+export * from './out/animal';
+//
+export * from './in/baja';
+export * from './mongo/baja';
+export * from './out/baja';
+//
+export * from './in/categoria';
+export * from './mongo/categoria';
+export * from './out/categoria';
+//
+export * from './in/corral';
+export * from './mongo/corral';
+export * from './out/corral';
+//
+export * from './in/diagnostico';
+export * from './mongo/diagnostico';
+export * from './out/diagnostico';
+//
+export * from './in/especie';
+export * from './mongo/especie';
+export * from './out/especie';
+//
+export * from './in/establecimiento';
+export * from './mongo/establecimiento';
+export * from './out/establecimiento';
+//
+export * from './in/eventoEspecifico';
+export * from './mongo/eventoEspecifico';
+export * from './out/eventoEspecifico';
+//
+export * from './in/grupo';
+export * from './mongo/grupo';
+export * from './out/grupo';
+//
+export * from './in/lote-animal';
+export * from './mongo/lote-animal';
+export * from './out/lote-animal';
+//
+export * from './in/lote-silobolsa';
+export * from './mongo/lote-silobolsa';
+export * from './out/lote-silobolsa';
+//
+export * from './in/pesaje';
+export * from './mongo/pesaje';
+export * from './out/pesaje';
+//
+export * from './in/raza';
+export * from './mongo/raza';
+export * from './out/raza';
+//
+export * from './in/servicio';
+export * from './mongo/servicio';
+export * from './out/servicio';
+//
+export * from './in/silobolsa';
+export * from './mongo/silobolsa';
+export * from './out/silobolsa';
+//
+export * from './in/subcategoria';
+export * from './mongo/subcategoria';
+export * from './out/subcategoria';
+//
+export * from './in/tipoBaja';
+export * from './mongo/tipoBaja';
+export * from './out/tipoBaja';
+//
+export * from './in/tipoTratamiento';
+export * from './mongo/tipoTratamiento';
+export * from './out/tipoTratamiento';
+//
+export * from './in/tipoVacuna';
+export * from './mongo/tipoVacuna';
+export * from './out/tipoVacuna';
+//
+export * from './in/tratamiento';
+export * from './mongo/tratamiento';
+export * from './out/tratamiento';
+//
+export * from './in/vacunacion';
+export * from './mongo/vacunacion';
+export * from './out/vacunacion';
+//
---
+++ src/modelos/shared/index.ts
@@ -0,0 +1,19 @@
+export * from './ack';
+export * from './uplink';
+export * from './downlink';
+export * from './metadatos';
+export * from './coordenadas';
+export * from './token';
+export * from './filtro';
+//
+// export * from './in/dispositivo';
+// export * from './mongo/dispositivo';
+// export * from './out/dispositivo';
+//
+export * from './in/comando';
+export * from './mongo/comando';
+export * from './out/comando';
+//
+export * from './in/log-dispositivo';
+export * from './mongo/log-dispositivo';
+export * from './out/log-dispositivo';
