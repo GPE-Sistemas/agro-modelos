4b661f252a51839f96df738c16222dffffaf9577
Fernando Cortese <cortesefernando@gmail.com>
2021-05-14 11:44:17 -0300

    alerta fecha opcional

--- src/modelos/shared/in/alerta.ts
+++ src/modelos/shared/in/alerta.ts
@@ -80,7 +80,7 @@ export interface IAlerta {
         usuario: string;
         comentario: string;
     }[];
-    fecha: string;
+    fecha?: string;
     estadoActual?: string;
     estados?: {
         fecha: string;
