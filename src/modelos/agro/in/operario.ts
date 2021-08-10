965bf04d1870299925ea7370f26302e9715d0524
Fernando Cortese <cortesefernando@gmail.com>
2021-08-10 15:51:47 -0300

    operario extends usuario

--- src/modelos/agro/in/operario.ts
+++ src/modelos/agro/in/operario.ts
@@ -1,15 +1,16 @@
 import joi from 'joi';
 import j2s from 'joi-to-swagger';
+import { IUsuario, IUsuarioValidation } from '../../admin';
 import { IPermiso, IPermisoValidation } from './permisos';
 
-export const IOperarioValidation = joi.object<IOperario>({
+export const IOperarioValidation = (IUsuarioValidation as joi.ObjectSchema<IOperario>).keys({
     idUsuario: joi.string(),
     permisos: IPermisoValidation,
 });
 
 export const IOperarioSwagger = j2s(IOperarioValidation).swagger;
 
-export interface IOperario {
+export interface IOperario extends IUsuario {
     idUsuario?: string;
     permisos?: IPermiso;
 }
